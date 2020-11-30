bcb646e1daa0e6a48a999f126cc8c24f3741f142
Omar Belkady <31806568+omarbelkady@users.noreply.github.com>
2020-11-30 20:30:29 +0000

    a

--- shopping_cart/src/App.js
+++ shopping_cart/src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
 		  	<hr/>
 		  	<div className="row">
 				<div className="col-md-8">
-				<Filter size={this.state.size} sort={this.state.sort} handleChangeSize={this.handleChangeSize} handleChangeSource={this.handleChangeSort} count={this.state.filteredProducts.length} />
+				<Filter size={this.state.size} sort={this.state.sort} handleChangeSize={this.handleChangeSize} handleChangeSort={this.handleChangeSort} count={this.state.filteredProducts.length} />
 				<hr/>
 					<Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart}/>
 				</div>
